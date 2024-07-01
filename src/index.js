fbc081d09eb56d0426f2d42e031c0ab657fbfec2
Gaurav <gaurav.bhe.24@gmail.com>
2024-07-01 18:51:35 +0530

    added index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+// console.log("Gaurav");
 const express = require ('express');
 const { PORT } = require('./config/ServerConfig');
+
 const bodyParser = require('body-parser');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async () => {
 const app = express();
@@ -9,10 +12,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use('/api', ApiRoutes);
+
 app.listen(PORT, () => {
     console.log(`Server is running ${PORT}`);
     // console.log(process.env);
 });
 }
 setupAndStartServer();
-console.log("Gaurav");
+// console.log("Gaurav");
+
+
